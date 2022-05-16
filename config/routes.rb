1dcb1c81d7f8baaefcec9d49cdfd16c32d52b671
TimmyChan99 <felemenoun@gmail.com>
2022-05-16 17:37:11 +0100

    Set up routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#new"
+  root "users#index"
+
+  resources :recipes, except: [:edit, :update]
 end
