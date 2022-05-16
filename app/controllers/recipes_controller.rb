1c5af5ed5c049c1e407b6c712578f13ec37aa031
TimmyChan99 <felemenoun@gmail.com>
2022-05-16 21:41:22 +0100

    Create recipes#show view and display data

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
 
 	def show
 		@recipe = Recipe.find(params[:id])
+		@recipe_foods = RecipeFood.where(['recipe_id = :id', { id: params[:id].to_s }])
 	end
 
 	def new
