9c96b655b22b4fb1712e159543390be724e89688
TimmyChan99 <felemenoun@gmail.com>
2022-05-17 13:20:00 +0100

    Allow user to create a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,11 +8,38 @@ class RecipesController < ApplicationController
     @recipe_foods = RecipeFood.where(['recipe_id = :id', { id: params[:id].to_s }])
   end
 
-  def new; end
+  def new
+  @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+  
+    if @recipe.save
+      flash[:notice] = 'Recipe created successfully'
+      redirect_to recipes_path
+    else
+      flash[:alert] = 'Error: Could not create a recipe'
+      redirect_to new_recipe_path
+    end
+  end
 
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
     redirect_to recipes_path
   end
+
+  private
+
+  def to_boolean
+    self == 'true'
+  end
+
+  def recipe_params
+    p = params.require(:post).permit(:name, :cooking_time, :preparation_time, :description, :public)
+    p[:user_id] = current_user.id
+    p
+  end
+  
 end
