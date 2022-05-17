d0afb5eb6d4b847f56ca436872a6f5024497cb27
TimmyChan99 <felemenoun@gmail.com>
2022-05-17 11:07:01 +0100

    Display list of recipes created by the logged-in user in recipes#show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    current_user = 1
-    @recipes = Recipe.where(['user_id = :id', { id: current_user.to_s }])
+    @recipes = Recipe.where(['user_id = :id', { id: current_user.id.to_s }])
   end
 
   def show
