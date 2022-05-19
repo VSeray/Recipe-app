0750b567c14b7e4b0210b1eff947a7fe4494c2cc
TimmyChan99 <felemenoun@gmail.com>
2022-05-19 13:48:26 +0100

    Add test for shopping list request

--- spec/requests/shopping_list_spec.rb
+++ spec/requests/shopping_list_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'ShoppingLists', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+  
+  let(:user) { User.create(name: 'Cork', email: 'example@mail.com', password: 'password') }
+
+  describe 'Test Get shopping_list#index' do
+    before(:each) do
+      sign_in user
+      get '/general_shopping_list/'
+    end
+    it 'Test If the correct template was rendered' do
+      expect(response).to render_template(:index)
+    end
+    it 'Test If response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Test If the response body includes correct placeholder text' do
+      expect(response.body).to include('Shopping List')
+    end
   end
 end
